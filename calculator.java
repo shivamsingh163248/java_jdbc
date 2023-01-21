f0d2de2c96756317704cdbeae8727926dcd7c940
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-01-21 19:53:13 +0530

    now we are the changes

--- calculator.java
+++ calculator.java
@@ -13,6 +13,7 @@ public static void main(String[] args) {
     System.out.println("4:- multipy");
     Scanner input =  new Scanner(System.in);
     int a = input.nextInt();
+    
     switch (a) {
         case 1:
             System.out.println("you are the selected add calulation :-"+a);
@@ -31,11 +32,41 @@ public static void main(String[] args) {
             d = input.nextInt();
             f = c-d;
             System.out.println("your output is : "+f);
-    
+            break;
+        case 3:
+            System.out.println("you are the seleted divide :- "+a);
+            System.out.println("please enter the frist value ");
+            c = input.nextInt();
+            System.out.println("please enter th value second");
+            d = input.nextInt();
+            f = c/d;
+            System.out.println("your output is : "+f);
+            break;
+        
+        case 4 :
+            System.out.println("you are the selected the multipliccation : - "+a);
+            System.out.println("please enter the frist value ");
+            c = input.nextInt();
+            System.out.println("please enter th value second");
+            d = input.nextInt();
+            f = c*d;
+            System.out.println("your output is : "+f);
+            break;
+        
+
         default:
         System.out.println("you are the not any specific input value");
+
             break;
     }
+
+  do {
+    System.out.println("fristly we done the do program then we chack the condaton:-"+a);
+    a++;
+    System.out.println("again chack the condation value"+a);
+    
+
+  } while (a > 4 );
     
 input.close();
 }  
