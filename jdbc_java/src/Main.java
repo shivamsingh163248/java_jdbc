2b286a791858ed4067ddde27d6d3e55539fad134
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-12-28 08:46:23 +0530

    update

--- jdbc_java/src/Main.java
+++ jdbc_java/src/Main.java
@@ -5,7 +5,9 @@ import java.sql.*;
 
 public class Main {
 
-
+     private static final  String url = "jdbc:mysql://localhost:3307/?user=root";
+     private static   String username = "root" ;
+     private static   String password = "";
 
     public static void main(String[] args) {
 
@@ -15,6 +17,18 @@ public class Main {
         }catch( ClassNotFoundException e){
             System.out.println(e.getMessage());
         }
+
+
+        // now creating the connection between the deta base
+
+        try {
+            Connection connection = DriverManager.getConnection(url , username , password);
+            Statement statement = connection.createStatement();
+
+        }catch (SQLException e){
+            System.out.println(e.getMessage());
+        }
+
 
 
 
