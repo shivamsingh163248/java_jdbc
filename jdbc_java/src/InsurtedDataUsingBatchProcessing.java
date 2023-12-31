002dce62056d099c56f8270d81bf91a14a9e2cd2
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-12-31 07:11:30 +0530

    update

--- jdbc_java/src/InsurtedDataUsingBatchProcessing.java
+++ jdbc_java/src/InsurtedDataUsingBatchProcessing.java
@@ -1,4 +1,14 @@
 public class InsurtedDataUsingBatchProcessing {
 
+    // now creating the method and used the batch processing system
+
+    public void InsertUsingBatchProcessing( String url , String username , String password){
+
+        // creating the connection after the loding the drive manager
+
+
+
+    }
+
 
 }
