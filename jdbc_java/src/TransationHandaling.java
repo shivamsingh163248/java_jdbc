b0d196291c9a750bab41cb66e61020c31fcf9e26
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-01-03 02:38:11 +0530

    update

---
+++ jdbc_java/src/TransationHandaling.java
@@ -0,0 +1,14 @@
+import java.sql.Connection;
+
+public class TransationHandaling {
+
+
+    // now creating the function of the transation handaling
+
+    public void transation (Connection connection){
+
+        // noww creating the query
+        String query =
+    }
+
+}
