58b35c42f3f0054902d70ac6bb89276440a82aa7
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-01-03 17:02:40 +0530

    update

--- jdbc_java/src/TransationHandaling.java
+++ jdbc_java/src/TransationHandaling.java
@@ -5,48 +5,6 @@ import java.sql.SQLException;
 
 public class TransationHandaling {
 
-
-    // now creating the function of the transation handaling
-
-    public void transation (Connection connection){
-
-        // noww creating the query
-        String Depited = "UPDATE  ACCOUNT SET balance = balance - ? WHERE account_number = ?" ;
-        String credited = "UPDATE  ACCOUNT SET balance = balance + ? WHERE account_number = ?";
-
-        // now creating the pri paired statement
-        try {
-            PreparedStatement debitpPreparedStatement = connection.prepareStatement(Depited) ;
-            PreparedStatement creditPreparedStatement = connection.prepareStatement(credited) ;
-            connection.setAutoCommit(false);
-
-            // creating the set the number
-            debitpPreparedStatement.setInt(1,500);
-            debitpPreparedStatement.setInt(2,101);
-            creditPreparedStatement.setInt(1,500);
-            creditPreparedStatement.setInt(2,102);
-
-            // now excution the statement
-            int debit = debitpPreparedStatement.executeUpdate();
-            int credite = creditPreparedStatement.executeUpdate();
-
-
-            // now calling the function
-            if (Issufficent(connection , 101 , 500)){
-                connection.commit();
-            }else{
-                connection.rollback();
-            }
-
-
-
-
-
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
     public boolean Issufficent(Connection connection, int account_numbr , double balance) {
 
         // now making the query for the pri paired statement
@@ -77,7 +35,55 @@ public class TransationHandaling {
             throw new RuntimeException(e);
         }
 
+
+
         return true ;
     }
+
+
+
+    // now creating the function of the transation handaling
+
+    public void transation (Connection connection){
+
+        // noww creating the query
+        String Depited = "UPDATE  ACCOUNT SET balance = balance - ? WHERE account_number = ?" ;
+        String credited = "UPDATE  ACCOUNT SET balance = balance + ? WHERE account_number = ?";
+
+        // now creating the pri paired statement
+        try {
+            PreparedStatement debitpPreparedStatement = connection.prepareStatement(Depited) ;
+            PreparedStatement creditPreparedStatement = connection.prepareStatement(credited) ;
+            connection.setAutoCommit(false);
+
+            // creating the set the number
+            debitpPreparedStatement.setDouble(1,4);
+            debitpPreparedStatement.setInt(2,101);
+            creditPreparedStatement.setDouble(1,200);
+            creditPreparedStatement.setInt(2,102);
+
+            // now excution the statement
+            int debit = debitpPreparedStatement.executeUpdate();
+            int credite = creditPreparedStatement.executeUpdate();
+
+
+            // now calling the function
+
+
+            if (Issufficent(connection , 101 , 4)){
+                connection.commit();
+            }else{
+                connection.rollback();
+            }
+
+
+
+
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 
 }
--- jdbc_java/src/transation.java
+++ jdbc_java/src/transation.java
@@ -30,5 +30,7 @@ public class transation {
         check.transation(connection);
 
 
+
+
     }
 }
